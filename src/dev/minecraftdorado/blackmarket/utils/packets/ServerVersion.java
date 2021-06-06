c71682489aa8fdcdc005dd247787a5d4b17b960e
Bruno Agostini <contacto@brunoagostini.com>
2021-06-06 20:47:35 -0300

    UMaterial updated

--- src/dev/minecraftdorado/blackmarket/utils/packets/ServerVersion.java
+++ src/dev/minecraftdorado/blackmarket/utils/packets/ServerVersion.java
@@ -21,7 +21,10 @@ public class ServerVersion {
 	
 	public interface Versionable {
 	    String VERSION = Bukkit.getVersion();
-	    boolean EIGHT = VERSION.contains("1.8"), NINE = VERSION.contains("1.9"), TEN = VERSION.contains("1.10"), ELEVEN = VERSION.contains("1.11"), TWELVE = VERSION.contains("1.12"), THIRTEEN = VERSION.contains("1.13"), FOURTEEN = VERSION.contains("1.14"), FIFTEEN = VERSION.contains("1.15"), SIXTEEN = VERSION.contains("1.16");
-	    boolean LEGACY = EIGHT || NINE || TEN || ELEVEN || TWELVE;
+	    boolean EIGHT = VERSION.contains("1.8");
+	    boolean NINE = VERSION.contains("1.9");
+	    boolean TEN = VERSION.contains("1.10");
+	    boolean ELEVEN = VERSION.contains("1.11");
+	    boolean TWELVE = VERSION.contains("1.12");
 	}
 }
