a8f62542d7ee9cbd2e0e16f541f9bce3dc9ff463
Bruno Agostini <contacto@brunoagostini.com>
2022-04-03 17:53:17 -0300

    .sql modified Bugs with sql solved

--- resources/sql/blackmarket.sql
+++ resources/sql/blackmarket.sql
@@ -1,4 +1,4 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";;
 
 
 /*
@@ -8,53 +8,53 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
  */
 
 /* ITEM STATUS */
-create table if not exists blackmarket_item_status
+CREATE TABLE IF NOT EXISTS blackmarket_item_status
 (
     status_id serial,
     descript varchar(255) not null
-) ENGINE =InnoDB DEFAULT CHARSET = utf8mb4;
+) ENGINE =InnoDB DEFAULT CHARSET = utf8mb4;;
 
-insert ignore into blackmarket_item_status (status_id, descript)
-values
+INSERT IGNORE INTO blackmarket_item_status (status_id, descript)
+VALUES
     (1, 'ON_SALE'),
     (2, 'SOLD'),
     (3, 'TAKED'),
-    (4, 'TIME_OUT');
+    (4, 'TIME_OUT');;
 
 
 /* ITEM DATA */
-create table if not exists blackmarket_items
+CREATE TABLE IF NOT EXISTS blackmarket_items
 (
     id serial,
     user varchar(36) not null,
     expiration_date timestamp not null,
     status_id bigint unsigned default 1 not null,
-    price decimal(7,2) not null, -- million
+    price decimal(7,2) not null, /* million */
     notified boolean default false not null,
 
     foreign key (status_id) references blackmarket_item_status(status_id)
-) ENGINE =InnoDB DEFAULT CHARSET = utf8mb4;
+) ENGINE =InnoDB DEFAULT CHARSET = utf8mb4;;
 
-create table if not exists blackmarket_item_data
+CREATE TABLE IF NOT EXISTS blackmarket_item_data
 (
     item_id bigint not null references blackmarket_items(id),
     item_data longtext not null,
     amount tinyint not null,
     material varchar(50) not null
-) ENGINE =InnoDB DEFAULT CHARSET = utf8mb4;
+) ENGINE =InnoDB DEFAULT CHARSET = utf8mb4;;
 
-create table if not exists blackmarket_item_content
+CREATE TABLE IF NOT EXISTS blackmarket_item_content
 (
     item_id bigint not null references blackmarket_items(id),
     item_data longtext not null
-) ENGINE =InnoDB DEFAULT CHARSET = utf8mb4;
+) ENGINE =InnoDB DEFAULT CHARSET = utf8mb4;;
 
 /* CATEGORIES */
-create table blackmarket_category
+CREATE TABLE IF NOT EXISTS blackmarket_category
 (
     category varchar(50) not null,
     materials longtext not null
-) ENGINE =InnoDB DEFAULT CHARSET = utf8mb4;
+) ENGINE =InnoDB DEFAULT CHARSET = utf8mb4;;
 
 
 
@@ -71,7 +71,7 @@ BEGIN
 
     DECLARE _item_id BIGINT;
 
-    IF (_post_limit <> -1 and (select count(*) from blackmarket_items where user = _seller and status_id = 1) >= _post_limit) THEN
+    IF (_post_limit <> -1 AND (SELECT count(*) FROM blackmarket_items WHERE user = _seller AND status_id = 1) >= _post_limit) THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Post limit';
     END IF;
 
@@ -80,40 +80,40 @@ BEGIN
 
     INSERT INTO blackmarket_item_data(item_id, item_data, material, amount) VALUE (_item_id, _item, substring_index(_item, ' ', 1), substring_index(substring_index(_item, ' ', 2), ' ', -1));
 
-    if _content is not null then
+    IF _content IS NOT NULL THEN
         INSERT INTO blackmarket_item_content(item_id, item_data) VALUE (_item_id, _content);
-    end if;
+    END IF;
 
-END;
+END;;
 
 /* LOAD ITEMS */
 CREATE PROCEDURE bm_load_items (_user VARCHAR(36), _status VARCHAR(255), _category VARCHAR(50), _order_type VARCHAR(20), _invert BOOLEAN, _limit SMALLINT, _offset BIGINT)
 BEGIN
 
-    update blackmarket_items set status = 4 where status = 1 and expiration_date < now();
+    UPDATE blackmarket_items SET status_id = 4 WHERE status_id = 1 AND expiration_date < now();
 
     SET _order_type = (
         CASE _order_type
-            WHEN 'amount' THEN 'item_id'
+            WHEN 'amount' THEN 'amount'
             WHEN 'value' THEN 'price'
-            WHEN 'material' THEN 'item_id'
+            WHEN 'material' THEN 'data.material'
             ELSE 'item_id'
             END);
 
     SET @invert_str = IF(_invert, ' desc ', ' asc ');
 
-    set @query = concat(
+    SET @query = concat(
             'select data.item_id, user, data.item_data, content.item_data content, price, status.descript status, expiration_date '
         , 'from blackmarket_items item '
         , 'join blackmarket_item_data data on item.id = data.item_id '
-        , 'join blackmarket_item_status status on item.status = status.status_id ');
+        , 'join blackmarket_item_status status on item.status_id = status.status_id ');
 
-    IF(_category is not null and _category <> 'all') THEN
+    IF(_category IS NOT NULL AND _category <> 'all') THEN
         SET @query = concat(@query
             , 'join blackmarket_category category on category.category = ', QUOTE(_category) , ' and data.material in (category.materials) ');
-    end if;
+    END IF;
 
-    set @query = concat(@query
+    SET @query = concat(@query
         , 'left join blackmarket_item_content content on item.id = content.item_id and data.item_data like ', QUOTE('%SHULKER_BOX%'), ' '
         , 'where notified = false and status.descript = ', QUOTE(_status), ' and (', QUOTE(_user), ' is null or user = ', QUOTE(_user), ') '
 
@@ -124,24 +124,24 @@ BEGIN
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 
-END;
+END;;
 
 /* LOAD ITEMS SIZE */
 CREATE PROCEDURE bm_load_items_size (_user VARCHAR(36), _status VARCHAR(255), _category VARCHAR(50))
 BEGIN
 
-    set @query = concat(
+    SET @query = concat(
             'select count(*) size '
         , 'from blackmarket_items item '
         , 'join blackmarket_item_data data on item.id = data.item_id '
-        , 'join blackmarket_item_status status on item.status = status.status_id ');
+        , 'join blackmarket_item_status status on item.status_id = status.status_id ');
 
-    IF(_category is not null and _category <> 'all') THEN
+    IF(_category IS NOT NULL AND _category <> 'all') THEN
         SET @query = concat(@query
             , 'join blackmarket_category category on category.category = ', QUOTE(_category) , ' and data.material in (category.materials) ');
-    end if;
+    END IF;
 
-    set @query = concat(@query
+    SET @query = concat(@query
         , 'where notified = false and status.descript = ', QUOTE(_status), ' and (', QUOTE(_user), ' is null or user = ', QUOTE(_user), ') '
         , 'limit 1;');
 
@@ -149,4 +149,4 @@ BEGIN
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 
-END;
+END;;
