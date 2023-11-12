84eee2123bbdb474a2fdf69f4ab10789bde8e3a3
Agostini Bruno <bagostini@sancorseguros.com>
2023-11-12 13:25:34 -0300

    ProtocolLib
    - Interact at (npc interact)

--- src/dev/minecraftdorado/blackmarket/utils/packets/PacketReader.java
+++ src/dev/minecraftdorado/blackmarket/utils/packets/PacketReader.java
@@ -1,83 +1,49 @@
 package dev.minecraftdorado.blackmarket.utils.packets;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+
 import dev.minecraftdorado.blackmarket.mainclass.MainClass;
 import dev.minecraftdorado.blackmarket.utils.entities.npc.events.NPCInteractEvent;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.MessageToMessageDecoder;
 
 public class PacketReader {
 	
-	private static Class<?> CraftPlayer = Reflections.getOBCClass("entity.CraftPlayer");
-	private static HashMap<Player, reader> list = new HashMap<>();
-	
-	public static reader get(Player player) {
-		if(!list.containsKey(player))
-			list.put(player, new reader(player));
-		return list.get(player);
+	public PacketReader() {
+		 ProtocolLibrary.getProtocolManager().addPacketListener(interactAt());
 	}
-    
-    public static class reader {
-    	
-    	Player player;
-        Channel channel;
-    	
-        
-        public reader(Player player) {
-        	this.player = player;
-        	try {
-        		Object cp = Reflections.getHandle(CraftPlayer.cast(this.player));
-        		cp = Reflections.getField(cp, "playerConnection");
-        		cp = Reflections.getField(cp, "networkManager");
-        		this.channel = (Channel) Reflections.getField(cp, "channel");
-        	}catch(Exception ex) {
-        		ex.printStackTrace();
-        	}
-        }
-        
-        public void inject() {
-        	if(channel.pipeline() != null
-        			&& channel.pipeline().get("BMPacketInjector") == null)
-        		channel.pipeline().addAfter("decoder", "BMPacketInjector",new MessageToMessageDecoder<Object>() {
-        			@Override
-        			protected void decode(ChannelHandlerContext arg0,Object p,List<Object> arg2) throws Exception {
-        				arg2.add(p);readPacket(p);
-        			}
-        		});
-        }
-        
-        public void uninject(){
-        	if(channel.pipeline().get("BMPacketInjector") != null)
-        		channel.pipeline().remove("BMPacketInjector");
-        }
-        
-        public void readPacket(Object packet){
-        	if(packet.getClass().getSimpleName().equalsIgnoreCase("PacketPlayInUseEntity")){
-        		int id = (Integer)Reflections.getField(packet, "a");
-                
-                if(!Reflections.getField(packet, "action").toString().equals("INTERACT_AT")) return;
-                
-                ArrayList<Integer> list = new ArrayList<>();
-                if(MainClass.npcM.npcList.containsKey(player)) list = MainClass.npcM.npcList.get(player);
-                
-                if(list.contains(id)) return;
-                list.add(id);
-                MainClass.npcM.npcList.put(player, list);
-                
-                Bukkit.getScheduler().runTask(MainClass.main, () -> {
-                	if(MainClass.npcM.list.containsKey(id)) {
-                		NPCInteractEvent event = new NPCInteractEvent(player, MainClass.npcM.list.get(id));
-                		Bukkit.getPluginManager().callEvent(event);
-                	}
-                });
-        	}
-        }
-    }
+	
+	private PacketAdapter interactAt() {
+		return new PacketAdapter(MainClass.main, ListenerPriority.NORMAL, PacketType.Play.Client.USE_ENTITY) {
+			@Override
+			public void onPacketSending(PacketEvent e) {
+				PacketContainer packet = e.getPacket().deepClone();
+				int entityId = packet.getIntegers().read(0);
+				
+				Player player = e.getPlayer();
+				
+				ArrayList<Integer> list = new ArrayList<>();
+				if(MainClass.npcM.npcList.containsKey(player)) list = MainClass.npcM.npcList.get(player);
+				
+				if(list.contains(entityId)) return;
+				list.add(entityId);
+				MainClass.npcM.npcList.put(player, list);
+				
+				Bukkit.getScheduler().runTask(MainClass.main, () -> {
+					if(MainClass.npcM.list.containsKey(entityId)) {
+						NPCInteractEvent event = new NPCInteractEvent(player, MainClass.npcM.list.get(entityId));
+						Bukkit.getPluginManager().callEvent(event);
+					}
+				});
+			}
+		};
+	}
 }
