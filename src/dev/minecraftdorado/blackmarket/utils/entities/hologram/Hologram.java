ea5dc4f83be4929b248327c029c1177e0bb70127
Agostini Bruno <bagostini@sancorseguros.com>
2023-11-14 20:46:13 -0300

    Holograms without ProtocolLib
    Issue fixed: Only spawn one hologram

--- src/dev/minecraftdorado/blackmarket/utils/entities/hologram/Hologram.java
+++ src/dev/minecraftdorado/blackmarket/utils/entities/hologram/Hologram.java
@@ -1,93 +1,49 @@
 package dev.minecraftdorado.blackmarket.utils.entities.hologram;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
-import java.util.UUID;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.entity.EntityType;
+import org.bukkit.craftbukkit.v1_20_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_20_R1.util.CraftChatMessage;
 import org.bukkit.entity.Player;
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.wrappers.WrappedChatComponent;
-import com.comphenix.protocol.wrappers.WrappedDataValue;
-import com.comphenix.protocol.wrappers.WrappedDataWatcher;
+
+import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
+import net.minecraft.network.protocol.game.ClientboundRemoveEntitiesPacket;
+import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.entity.decoration.ArmorStand;
 
 public class Hologram {
 
 	private Location location;
 	private String text;
-	private final int entityId;
-	private final PacketContainer packetPlayOutSpawnEntityLiving;
-	private final PacketContainer packetPlayOutEntityDestroy;
-	private PacketContainer packetPlayEntityMetadata;
+	private ArmorStand armorstand;
 	private final Set<Player> viewers = new HashSet<>();
-	
-	private ProtocolManager protocolManager;
 
 	public Hologram(Location location, String text) {
-		this.protocolManager = ProtocolLibrary.getProtocolManager();
-		
 		this.location = location;
 		this.text = text;
-		this.entityId = UUID.randomUUID().variant();
 		
-		packetPlayOutSpawnEntityLiving = new PacketContainer(PacketType.Play.Server.SPAWN_ENTITY);
+		ServerLevel level = ((CraftWorld) location.getWorld()).getHandle();
 		
-		// Crucial data, do not remove
-		packetPlayOutSpawnEntityLiving.getIntegers().write(0, entityId);
-		packetPlayOutSpawnEntityLiving.getUUIDs().write(0, UUID.randomUUID());
-		packetPlayOutSpawnEntityLiving.getEntityTypeModifier().write(0, EntityType.ARMOR_STAND);
+		armorstand = new ArmorStand(level, location.getX(), location.getY() + 2, location.getZ());
+		armorstand.setInvisible(true);
+		armorstand.setNoGravity(true);
+		armorstand.setMarker(true);
 		
-		packetPlayOutSpawnEntityLiving.getDoubles()
-		.write(0, location.getX())
-		.write(1, location.getY())
-		.write(2, location.getZ());
-		
-		packetPlayOutEntityDestroy = new PacketContainer(PacketType.Play.Server.ENTITY_DESTROY);
-		packetPlayOutEntityDestroy.getIntLists().write(0, Arrays.asList(entityId));
-		
-		// Generete armorstand metadata
-		genereteMetadata();
+		armorstand.setCustomNameVisible(true);
+		setText(text);
 	}
-	
-	private void genereteMetadata() {
-		packetPlayEntityMetadata = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.ENTITY_METADATA);
-		packetPlayEntityMetadata.getIntegers().write(0, this.entityId);
-        
-        WrappedDataWatcher watcher = new WrappedDataWatcher();
-        
-        // Invisibility
-        watcher.setObject(new WrappedDataWatcher.WrappedDataWatcherObject(0, WrappedDataWatcher.Registry.get(Byte.class)), (byte) 0x20);
-        
-        // Custom name
-        Optional<?> name = Optional.of(WrappedChatComponent.fromChatMessage(this.text)[0].getHandle());
-        watcher.setObject(new WrappedDataWatcher.WrappedDataWatcherObject(2, WrappedDataWatcher.Registry.getChatComponentSerializer(true)), name);
-        
-        // Display custom name
-        watcher.setObject(new WrappedDataWatcher.WrappedDataWatcherObject(3, WrappedDataWatcher.Registry.get(Boolean.class)), true);
-        
-        // Set attributes
-        List<WrappedDataValue> wrappedDataValueList = new ArrayList<>();
-        watcher.getWatchableObjects().stream().filter(Objects::nonNull).forEach(entry -> {
-            WrappedDataWatcher.WrappedDataWatcherObject dataWatcherObject = entry.getWatcherObject();
-            wrappedDataValueList.add(new WrappedDataValue(dataWatcherObject.getIndex(), dataWatcherObject.getSerializer(), entry.getRawValue()));
-        });
-        packetPlayEntityMetadata.getDataValueCollectionModifier().write(0, wrappedDataValueList);
-    }
 
 	public void display(Player... players) {
 		for (Player player : players) {
 			if (viewers.add(player)) {
-				protocolManager.sendServerPacket(player, packetPlayOutSpawnEntityLiving);
-				protocolManager.sendServerPacket(player, packetPlayEntityMetadata);
+				ServerGamePacketListenerImpl ps = ((CraftPlayer) player).getHandle().connection;
+				ps.send(new ClientboundAddEntityPacket(armorstand));
+				ps.send(new ClientboundSetEntityDataPacket(getEntityId(), armorstand.getEntityData().getNonDefaultValues()));
 			}
 		}
 	}
@@ -95,8 +51,9 @@ public class Hologram {
 	public void display() {
 		for (Player player : Bukkit.getOnlinePlayers()) {
 			if (viewers.add(player)) {
-				protocolManager.sendServerPacket(player, packetPlayOutSpawnEntityLiving);
-				protocolManager.sendServerPacket(player, packetPlayEntityMetadata);
+				ServerGamePacketListenerImpl ps = ((CraftPlayer) player).getHandle().connection;
+				ps.send(new ClientboundAddEntityPacket(armorstand));
+				ps.send(new ClientboundSetEntityDataPacket(getEntityId(), armorstand.getEntityData().getNonDefaultValues()));
 			}
 		}
 	}
@@ -104,21 +61,23 @@ public class Hologram {
 	public void hide(Player... players) {
 		for (Player player : players) {
 			if (viewers.remove(player)) {
-				protocolManager.sendServerPacket(player, packetPlayOutEntityDestroy);
+				ServerGamePacketListenerImpl ps = ((CraftPlayer) player).getHandle().connection;
+				ps.send(new ClientboundRemoveEntitiesPacket(getEntityId()));
 			}
 		}
 	}
 	
 	public void hide() {
 		viewers.forEach(player ->{
-			protocolManager.sendServerPacket(player, packetPlayOutEntityDestroy);
+			ServerGamePacketListenerImpl ps = ((CraftPlayer) player).getHandle().connection;
+			ps.send(new ClientboundRemoveEntitiesPacket(getEntityId()));
 		});
 		viewers.clear();
 	}
 	
 	public void setText(String text) {
 		this.text = text;
-		genereteMetadata();
+		armorstand.setCustomName(CraftChatMessage.fromString(text, true)[0]);
 	}
 
 	public Location getLocation() {
@@ -127,6 +86,10 @@ public class Hologram {
 
 	public String getText() {
 		return text;
+	}
+	
+	public int getEntityId() {
+		return armorstand.getId();
 	}
 	
 }
